c39e31e8da068f615e13561263d6f34ecc4f223f
Nikolas Lenning <nikolaslenning@gmail.com>
2020-12-30 11:46:03 -0700

    deprecated update

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import { createBrowserHistory as createHistory } from "history";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+const history = createHistory();
+
+function App({ calendarStore }) {
+  return (
+    <div>
+      <Router history={history}>
+        <Navbar bg="primary" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">Calendar App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Route
+          path="/"
+          exact
+          component={props => (
+            <HomePage {...props} calendarStore={calendarStore} />
+          )}
+        />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
